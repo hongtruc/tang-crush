e12ccf29c6521dc59e0e8368e4363ea0afa2bc4a
hongtruc <86900515+hongtruc@users.noreply.github.com>
2021-07-04 10:07:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Anh à!',
+    introTitle: 'Chinh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao đồng chí cứ đi nhầm đường
+    Lạc vào tim toi lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, Em gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Anh lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    title: 'Phải chăng Chinh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Chinh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích New <33',
+    btnNo: 'Không, Đồng chí à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Chinh lại yêu toii <3',
+    btnReply: 'Gửi cho New <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    mess: 'Toi biết mà 🥰. Yêu đồng chí nhiều nhiều 😘😘',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
